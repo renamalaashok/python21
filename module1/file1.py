bae36fe749635de6f46438f2780df7c532e35ca1
sambapython@gmail.com <sambapython@gmail.com>
2016-11-26 14:04:31 +0530

    2016-NOV-26

---
+++ module1/file1.py
@@ -0,0 +1,5 @@
+print "program started"
+def fun():
+	return "this is fun in file1"
+print "other statements in program"
+print "program ended"
---
+++ module1/py_file.py
@@ -0,0 +1,10 @@
+"""
+import file1
+import sqlite3
+con = sqlite3.connect('db1.db')
+"""
+import mod2
+print mod2.f1.fun()
+print mod2.f2.fun()
+import sqlite3
+con = sqlite3.connect('db1.db')
